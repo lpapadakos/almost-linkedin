7c9ca7a7e43058051944e6da8931d17a3380f73a
Leonidas P. Papadakos <papadakospan@gmail.com>
2021-05-13 22:14:49 +0300

    Add navbar, using bootstrap for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, HttpClientModule],
+	declarations: [AppComponent, WelcomeComponent, RegisterComponent],
+	imports: [BrowserModule, HttpClientModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
